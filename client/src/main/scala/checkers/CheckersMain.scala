f37a3f94725c605e8affd9d471f1bcbac123e07d
Kevin Schuetz <schuetzk@gmail.com>
2016-06-10 00:16:21 -0500

    Removed modules package

--- client/src/main/scala/checkers/CheckersMain.scala
+++ client/src/main/scala/checkers/CheckersMain.scala
@@ -3,9 +3,7 @@ package checkers
 import checkers.driver.GameScreenDriver
 import checkers.logger._
 import checkers.models.{GameScreenModel, GameSettings}
-import checkers.modules.Sandbox
 import checkers.style.GlobalStyles
-import japgolly.scalajs.react.ReactDOM
 import org.scalajs.dom
 
 import scala.scalajs.js
@@ -24,14 +22,10 @@ object CheckersMain extends js.JSApp {
     GlobalStyles.addToDocument()
 
     val host = dom.document.getElementById("root")
-    sandbox2(host)
+    sandbox1(host)
   }
 
   private def sandbox1(host: dom.Node): Unit = {
-    ReactDOM.render(Sandbox.test, host)
-  }
-
-  private def sandbox2(host: dom.Node): Unit = {
     val model = GameScreenModel.initial(GameSettings.default)
     val driver = new GameScreenDriver(host, model)
     driver.run()
