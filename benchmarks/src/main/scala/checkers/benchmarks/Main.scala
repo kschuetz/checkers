85b949b715735cb162a8ce4f0a896f08f314a197
Kevin Schuetz <schuetzk@gmail.com>
2016-09-08 00:18:58 -0500

    Cleanup

--- benchmarks/src/main/scala/checkers/benchmarks/Main.scala
+++ benchmarks/src/main/scala/checkers/benchmarks/Main.scala
@@ -1,8 +1,8 @@
 package checkers.benchmarks
 
 import checkers.benchmarks.suites.Sandbox
-import org.scalajs.dom.document
 import japgolly.scalajs.benchmark.gui.BenchmarkGUI
+import org.scalajs.dom.document
 
 import scala.scalajs.js.annotation.JSExport
 
@@ -10,11 +10,6 @@ import scala.scalajs.js.annotation.JSExport
 object Main extends scalajs.js.JSApp {
 
   def main(): Unit = {
-
-    // import concurrent.duration._
-    // import japgolly.scalajs.benchmark.engine.Options
-    // val opts = Options.Default.copy(minRuns = 1000, minTime = 0.millis)
-
     val tgt = document.getElementById("body")
 
     BenchmarkGUI.renderMenu(tgt)(Sandbox.suite)
