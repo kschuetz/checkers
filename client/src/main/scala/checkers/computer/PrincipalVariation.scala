211b519a5bb7d7296528cacb6f24f7d9746c3a3e
Kevin Schuetz <schuetzk@gmail.com>
2016-09-13 22:17:35 -0500

    PrincipalVariation

---
+++ client/src/main/scala/checkers/computer/PrincipalVariation.scala
@@ -0,0 +1,37 @@
+package checkers.computer
+
+import scala.scalajs.js
+
+class PrincipalVariation[A](val depth: Int) {
+  val size = depth * depth
+  val line = initLine(size)
+
+  def getBestMove(ply: Int): A = line(ply)
+
+  def updateBestMove(ply: Int, value: A): Unit = {
+    val baseIndex = ply * depth
+    line(baseIndex) = value
+    if(ply < depth - 1) {
+      var source = baseIndex + 1
+      var dest = baseIndex + depth
+      var copyCount = depth - ply - 1
+
+      while(copyCount > 0) {
+        line(dest) = line(source)
+        source += 1
+        dest += 1
+        copyCount -= 1
+      }
+    }
+  }
+
+  private def initLine(size: Int): js.Array[A] = {
+    val result = new js.Array[A](size)
+    var i = 0
+    while(i < size) {
+      result(i) = null.asInstanceOf[A]
+      i += 1
+    }
+    result
+  }
+}
