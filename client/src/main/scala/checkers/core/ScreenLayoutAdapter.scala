b71b98f52a2432bab159c5401ec5fe977f5708c2
Kevin Schuetz <schuetzk@gmail.com>
2016-12-03 17:52:22 -0600

    ScreenLayoutAdapter now has four levels

--- client/src/main/scala/checkers/core/ScreenLayoutAdapter.scala
+++ client/src/main/scala/checkers/core/ScreenLayoutAdapter.scala
@@ -8,6 +8,48 @@ class ScreenLayoutAdapter extends ScreenLayoutSettingsProvider {
 
   private var cache: CacheSettings = _
 
+  private val thresholds = Vector(
+    (1024, 896),
+    (924, 796),
+    (824, 696),
+    (724, 596))
+
+  private lazy val levels = Vector(
+    // large
+    new DefaultScreenLayoutSettings { },
+
+    new DefaultScreenLayoutSettings {
+      override def GameSceneWidthPixels: Int = 700
+
+      override def GameSceneHeightPixels: Int = 700
+    },
+
+    new DefaultScreenLayoutSettings {
+      override def GameSceneWidthPixels: Int = 600
+
+      override def GameSceneHeightPixels: Int = 600
+    },
+
+    new DefaultScreenLayoutSettings {
+      override def GameSceneWidthPixels: Int = 500
+
+      override def GameSceneHeightPixels: Int = 500
+    }
+  ).lift
+
+  private lazy val smallest = new DefaultScreenLayoutSettings {
+    override def GameSceneWidthPixels: Int = 400
+
+    override def GameSceneHeightPixels: Int = 400
+  }
+
+  private def getSettings(width: Int, height: Int): ScreenLayoutSettings = {
+    val idx = thresholds.indexWhere { case (minWidth, minHeight) =>
+      width >= minWidth && height >= minHeight
+    }
+    levels(idx).getOrElse(smallest)
+  }
+
   private lazy val largeScreen: ScreenLayoutSettings = new DefaultScreenLayoutSettings {}
 
   override def getScreenLayoutSettings(viewPortInfo: ViewPortInfo): ScreenLayoutSettings = {
@@ -16,7 +58,7 @@ class ScreenLayoutAdapter extends ScreenLayoutSettingsProvider {
 
     if(cache != null && cache.width == width && cache.height == height) cache.settings
     else {
-      val newSettings = largeScreen  // TODO
+      val newSettings = getSettings(width, height)
 
       println(s"ScreenLayoutAdapter:  width = $width, height = $height")
 
