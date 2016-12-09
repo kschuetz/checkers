531ea38ae96f7419aefc1e3459907d75a01a20fa
Kevin Schuetz <schuetzk@gmail.com>
2016-12-09 00:04:06 -0600

    More difficulty level tweaks

--- client/src/main/scala/checkers/computer/DifficultyLevels.scala
+++ client/src/main/scala/checkers/computer/DifficultyLevels.scala
@@ -3,9 +3,9 @@ package checkers.computer
 object DifficultyLevels {
   val LevelCount = 12
 
-  lazy val phase1MaxKCycles: Vector[Int] = Vector(50, 50, 50, 100, 100, 100, 200, 200, 400, 400, 800, 1600, 2000)
-  lazy val phase2MaxKCycles: Vector[Int] = Vector(50, 100, 150, 200, 250, 500, 800, 1000, 1200, 1500, 2000, 3000, 5000)
-  lazy val mainMaxKCycles: Vector[Int] = Vector(50, 100, 150, 250, 500, 1000, 2000, 4000, 6000, 8000, 10000, 15000, 20000)
+  lazy val phase1MaxKCycles: Vector[Int] = Vector(50, 75, 75, 200, 200, 200, 400, 400, 800, 800, 1600, 3200, 4000)
+  lazy val phase2MaxKCycles: Vector[Int] = Vector(50, 200, 300, 400, 500, 1000, 1600, 2000, 2400, 3000, 4000, 6000, 10000)
+  lazy val mainMaxKCycles: Vector[Int] = Vector(75, 200, 300, 500, 1000, 2000, 4000, 8000, 12000, 16000, 20000, 30000, 40000)
 
   lazy val phase1Weights: Vector[MoveSelectionMethodWeights] = Vector(
     weight(100, 5, 1),
@@ -24,32 +24,32 @@ object DifficultyLevels {
 
   lazy val phase2Weights: Vector[MoveSelectionMethodWeights] = Vector(
     weight(200, 7, 2),
-    weight(233, 7, 2),
-    weight(300, 7, 2),
-    weight(367, 7, 2),
-    weight(433, 6, 2),
-    weight(567, 6, 2),
-    weight(633, 6, 2),
-    weight(700, 5, 2),
-    weight(767, 5, 2),
-    weight(833, 4, 2),
-    weight(900, 4, 2),
-    weight(1000, 3, 1)
+    weight(467, 7, 2),
+    weight(600, 7, 2),
+    weight(800, 7, 2),
+    weight(933, 6, 2),
+    weight(1100, 6, 2),
+    weight(1267, 6, 2),
+    weight(1400, 5, 2),
+    weight(1550, 5, 2),
+    weight(1666, 4, 2),
+    weight(1800, 4, 2),
+    weight(2000, 3, 1)
   )
 
   lazy val mainWeights: Vector[MoveSelectionMethodWeights] = Vector(
     weight(200, 5, 2),
-    weight(233, 5, 2),
-    weight(300, 5, 2),
-    weight(367, 5, 2),
-    weight(433, 5, 2),
-    weight(567, 5, 2),
-    weight(633, 4, 2),
-    weight(700, 4, 2),
-    weight(767, 4, 2),
-    weight(833, 3, 2),
-    weight(900, 3, 2),
-    weight(1000, 3, 1)
+    weight(467, 5, 2),
+    weight(600, 5, 2),
+    weight(800, 5, 2),
+    weight(933, 5, 2),
+    weight(1100, 5, 2),
+    weight(1267, 4, 2),
+    weight(1400, 4, 2),
+    weight(1550, 4, 2),
+    weight(1666, 3, 2),
+    weight(1800, 3, 2),
+    weight(2000, 3, 1)
   )
 
 
